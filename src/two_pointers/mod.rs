14b2002b33d666ecf163c19dd9f13331cc01a36e
Valentin Terebenin <terebenin.work@gmail.com>
2023-07-27 21:28:22 +0300

    feat: added second task for two_sum

--- src/two_pointers/mod.rs
+++ src/two_pointers/mod.rs
@@ -1,8 +1,10 @@
 mod is_palindrome;
+mod two_sum_ii_array_is_sorted;
 use crate::utils::print_module_name;
 const MODULE_NAME: &str = "Two Pointers";
 
 pub fn main() {
     print_module_name(MODULE_NAME);
     is_palindrome::main();
+    two_sum_ii_array_is_sorted::main();
 }
---
+++ src/two_pointers/two_sum_ii_array_is_sorted.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+use crate::utils::print_pass;
+
+const NAME: &str = "two-sum-ii-input-array-is-sorted";
+const LINK: &str = "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/";
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut left = 0;
+    let mut right = numbers.len() - 1;
+    loop {
+        while numbers[left] + numbers[right] > target { 
+            right -= 1;
+        }
+        if numbers[left] + numbers[right] == target {
+            break vec![left as i32 + 1, right as i32 + 1];
+        } else { 
+            left += 1;
+        }
+    }
+}
+
+pub fn main() {
+    let items = vec![2, 3, 4];
+    let target = 6;
+    assert_eq!(two_sum(items, target), vec![1, 3]);
+    print_pass(NAME, LINK)
+}
