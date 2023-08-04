2ed88433c7e86704ebc28447bb7fb99fd3624d39
Valentin Terebenin <terebenin.work@gmail.com>
2023-08-04 16:05:48 +0300

    feat: added minimum in rotated sorted array rs

---
+++ src/binary_search/find_minimum_in_rotated_sorted_array.rs
@@ -0,0 +1,32 @@
+use crate::utils::print_pass;
+use std::cmp::Ordering;
+
+const NAME: &str = "find-minimum-in-rotated-sorted-array";
+const LINK: &str = "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/";
+
+pub fn find_min(nums: Vec<i32>) -> i32 {
+    if nums.len() == 1 { return *nums.first().unwrap(); }
+
+    let mut min = nums.first().unwrap();
+    let (mut low, mut high) = (0, nums.len());
+    while low < high {
+        let mid = low + (high - low) / 2;
+        match nums[mid].cmp(&min) {
+            Ordering::Less => {
+                min = &nums[mid];
+                high = mid;
+            },
+            Ordering::Greater => {
+                low = mid + 1;
+            },
+            Ordering::Equal => (),
+        }
+    }
+    *min
+}
+
+pub fn main() {
+    let nums: Vec<i32> = vec![3, 4, 5, 1, 2];
+    assert_eq!(find_min(nums), 1);
+    print_pass(NAME, LINK)
+}
--- src/binary_search/mod.rs
+++ src/binary_search/mod.rs
@@ -1,5 +1,6 @@
 mod binary_search;
 mod search_a_2d_matrix;
+mod find_minimum_in_rotated_sorted_array;
 
 use crate::utils::print_module_name;
 const MODULE_NAME: &str = "Binary Search";
@@ -8,4 +9,5 @@ pub fn main() {
     print_module_name(MODULE_NAME);
     binary_search::main();
     search_a_2d_matrix::main();
+    find_minimum_in_rotated_sorted_array::main();
 }
