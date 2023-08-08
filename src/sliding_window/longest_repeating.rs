caf89c0fcfcfb47f9141e88c62cd5047901023ee
Valentin Terebenin <terebenin.work@gmail.com>
2023-08-08 12:27:11 +0300

    feat: added longest repeating

---
+++ src/sliding_window/longest_repeating.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+use crate::utils::print_pass;
+
+const NAME: &str = "longest-repeating-character-replacement";
+const LINK: &str = "https://leetcode.com/problems/longest-repeating-character-replacement/";
+
+pub fn character_replacement(s: String, k: i32) -> i32 {
+    let mut count: HashMap<char, i32> = HashMap::new();
+
+    let mut res = 0;
+    let mut l = 0;
+    let mut maxf = 0;
+    for r in 0..s.len() {
+        let r_char = s.as_bytes()[r] as char;
+        let l_char = s.as_bytes()[l] as char;
+        *count.entry(r_char).or_insert(0) += 1;
+        maxf = maxf.max(*count.get(&r_char).unwrap());
+
+        while (r - l + 1) as i32 - maxf > k as i32 {
+            *count.entry(l_char).or_insert(0) -= 1;
+            l += 1;
+        }
+        res = res.max(r - l + 1);
+    }
+    res as i32
+}
+
+pub fn main() {
+    let s: String = String::from("ABAB");
+    let k: i32 = 2;
+    assert_eq!(character_replacement(s, k), 4);
+    let s: String = String::from("AABABBA");
+    let k: i32 = 1;
+    assert_eq!(character_replacement(s, k), 4);
+    let s: String = String::from("ABAA");
+    let k: i32 = 0;
+    assert_eq!(character_replacement(s, k), 2);
+    let s: String = String::from("ABBB");
+    let k: i32 = 2;
+    assert_eq!(character_replacement(s, k), 4);
+    print_pass(NAME, LINK)
+}
--- src/sliding_window/mod.rs
+++ src/sliding_window/mod.rs
@@ -1,6 +1,6 @@
 mod max_profit;
 mod longest_substring;
-
+mod longest_repeating;
 use crate::utils::print_module_name;
 const MODULE_NAME: &str = "Sliding Window";
 
@@ -8,4 +8,5 @@ pub fn main() {
     print_module_name(MODULE_NAME);
     max_profit::main();
     longest_substring::main();
+    longest_repeating::main();
 }
