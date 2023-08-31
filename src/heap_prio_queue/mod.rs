a3d4388f6c58f9363874901be2b002943ee11b63
Valentin Terebenin <terebenin.work@gmail.com>
2023-08-31 19:59:38 +0300

    feat: added stone game

--- src/heap_prio_queue/mod.rs
+++ src/heap_prio_queue/mod.rs
@@ -1,4 +1,5 @@
 mod kth_largest;
+mod stone_game;
 
 use crate::utils::print_module_name;
 const MODULE_NAME: &str = "Heap / Priority Queue";
@@ -6,4 +7,5 @@ const MODULE_NAME: &str = "Heap / Priority Queue";
 pub fn main() {
     print_module_name(MODULE_NAME);
     kth_largest::main();
+    stone_game::main();
 }
---
+++ src/heap_prio_queue/stone_game.rs
@@ -0,0 +1,32 @@
+use crate::utils::print_pass;
+use std::collections::BinaryHeap;
+
+const NAME: &str = "last-stone-weight";
+const LINK: &str = "https://leetcode.com/problems/last-stone-weight/";
+
+
+pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+    let mut heap: BinaryHeap<i32> = BinaryHeap::with_capacity(stones.len() + 1);
+    for stone in stones.iter() { 
+        heap.push(stone.to_owned());
+    }
+    while heap.len() > 1 {
+        let y = heap.pop().unwrap();
+        let x = heap.pop().unwrap();
+        if x != y {
+            heap.push(y - x);
+        }
+    }
+    heap.pop().unwrap_or(0)
+}
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * let obj = KthLargest::new(k, nums);
+ * let ret_1: i32 = obj.add(val);
+ */
+pub fn main() {
+    let stones = vec![2, 7, 4, 1, 8, 1];
+    let answer = 1;
+    assert_eq!(last_stone_weight(stones), answer);
+    print_pass(NAME, LINK);
+}
